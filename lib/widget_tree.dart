8d0bcaaa1a3d3d0300e5c28de8eca44f9b1b267e
SagitMalka <sagitmalka10@gmail.com>
2024-12-16 18:36:43 +0200

    first commit

---
+++ lib/widget_tree.dart
@@ -0,0 +1,28 @@
+import 'package:study_buddy/auth.dart';
+import 'package:study_buddy/pages/home_page.dart';
+import 'package:study_buddy/pages/login_register_page.dart';
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({super.key});
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges, 
+      builder: (context, snapshot) {
+        if(snapshot.hasData){
+          return HomePage();
+        }else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
