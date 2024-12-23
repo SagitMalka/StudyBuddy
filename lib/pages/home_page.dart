825cc55c0c79aec84adc48eab8ef0290d9401907
Sagit <sagitmalka10@gmail.com>
2024-12-23 06:49:51 -0800

    My Profile button moved to upper bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:study_buddy/widgets/user_info_widget.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
+
+  final User? user = FirebaseAuth.instance.currentUser;
 
   Widget _title() {
     return const Text('Study Buddy');
@@ -17,20 +20,24 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _navigateToProfileButton(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        Navigator.pushNamed(context, '/profile');
-      },
-      child: const Text('My Profile'),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: _title(),
+        actions: [
+          IconButton(
+            icon: CircleAvatar(
+              backgroundImage: user?.photoURL != null
+                  ? NetworkImage(user!.photoURL!)
+                  : null,
+              child: user?.photoURL == null ? const Icon(Icons.person) : null,
+            ),
+            onPressed: () {
+              Navigator.pushNamed(context, '/profile');
+            },
+          ),
+        ],
       ),
       body: Container(
         height: double.infinity,
@@ -42,8 +49,6 @@ class HomePage extends StatelessWidget {
           children: <Widget>[
             UserInfoWidget(),
             _navigateToCoursesButton(context),
-            const SizedBox(height: 10),
-            _navigateToProfileButton(context),
           ],
         ),
       ),
