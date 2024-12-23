d2a618a9581900e1d798e85e8c9365c9973a6714
Sagit <sagitmalka10@gmail.com>
2024-12-23 06:20:52 -0800

    user info widget

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,46 +1,28 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:study_buddy/auth.dart';
 import 'package:flutter/material.dart';
-import 'course_list_page.dart'; 
+import 'package:study_buddy/widgets/user_info_widget.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   HomePage({super.key});
-  final User? user = Auth().currentUser;
 
-
-  Future<void> signOut() async{
-    await Auth().signOut();
+  Widget _title() {
+    return const Text('Study Buddy');
   }
 
-  Widget _titel(){
-    return const Text('Firebase Auth');
-  }  
-  Widget _userUid(){
-    return Text(user?.email ?? 'user email');
-  }
-  Widget _signOutButton()  {
-    return ElevatedButton(
-      onPressed: signOut, 
-      child: const Text('Sign Out'),
-      );
-  }
   Widget _navigateToCoursesButton(BuildContext context) {
     return ElevatedButton(
       onPressed: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => CourseListPage()),
-        );
+        Navigator.pushNamed(context, '/courses');
       },
       child: const Text('View Courses'),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: _titel(),
-      ), //AppBar
+        title: _title(),
+      ),
       body: Container(
         height: double.infinity,
         width: double.infinity,
@@ -49,9 +31,8 @@ class HomePage extends StatelessWidget{
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            _userUid(),
-            _signOutButton(),
-            _navigateToCoursesButton(context), // Button to view courses
+            UserInfoWidget(),
+            _navigateToCoursesButton(context),
           ],
         ),
       ),
--- lib/widget_tree.dart
+++ lib/widget_tree.dart
@@ -1,6 +1,4 @@
 import 'package:study_buddy/auth.dart';
-import 'package:study_buddy/pages/home_page.dart';
-import 'package:study_buddy/pages/login_register_page.dart';
 import 'package:flutter/material.dart';
 
 class WidgetTree extends StatefulWidget {
@@ -8,20 +6,26 @@ class WidgetTree extends StatefulWidget {
 
   @override
   State<WidgetTree> createState() => _WidgetTreeState();
-
 }
 
 class _WidgetTreeState extends State<WidgetTree> {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: Auth().authStateChanges, 
+      stream: Auth().authStateChanges,
       builder: (context, snapshot) {
-        if(snapshot.hasData){
-          return HomePage();
-        }else {
-          return const LoginPage();
+        if (snapshot.hasData) {
+          // Navigate to HomePage if user is logged in
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            Navigator.pushReplacementNamed(context, '/home');
+          });
+        } else {
+          // Navigate to LoginPage if user is not logged in
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            Navigator.pushReplacementNamed(context, '/login');
+          });
         }
+        return const Center(child: CircularProgressIndicator());
       },
     );
   }
