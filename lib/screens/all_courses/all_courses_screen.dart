44d6693344727a1eac2ca9e59e8ec3e4319f90f0
ironmanlior <66523149+ironmanlior@users.noreply.github.com>
2025-01-03 16:52:55 +0200

    Add files via upload
    add search bar to view courses

--- lib/screens/all_courses/all_courses_screen.dart
+++ lib/screens/all_courses/all_courses_screen.dart
@@ -15,16 +15,34 @@ class _CourseListPageState extends State<CourseListPage> {
   final CourseService _courseService = CourseService();
   List<Map<String, dynamic>> _allCourses = [];
 
+  TextEditingController _searchController = TextEditingController();
+  List<Map<String, dynamic>> _filteredCourses = [];
+
   @override
   void initState() {
     super.initState();
     _loadCourses();
+
+    _searchController.addListener(_filterCourses);
   }
 
   Future<void> _loadCourses() async {
     final courses = await _courseService.fetchAllCourses();
     setState(() {
       _allCourses = courses;
+      _filteredCourses = courses;
+    });
+  }
+
+  void _filterCourses() {
+    final query = _searchController.text.toLowerCase();
+    setState(() {
+      _filteredCourses = _allCourses.where((course) => 
+        (course["name"]?.toLowerCase().contains(query) ?? false) ||
+        (course["major"]?.toLowerCase().contains(query) ?? false) ||
+        (course["instructor"]?.toLowerCase().contains(query) ?? false) ||
+        query.isEmpty
+      ).toList();
     });
   }
 
@@ -38,8 +56,21 @@ class _CourseListPageState extends State<CourseListPage> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            CourseListView(
-              allCourses: _allCourses,
+            TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: 'Search courses...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: CourseListView(
+                allCourses: _filteredCourses,
+              ),
             ),
             const SizedBox(height: 20),
             UploadCoursesButton()
