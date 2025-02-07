5a3787830119333a22b7f13d6676aaf2ad5c3d55
Sagit <sagitmalka10@gmail.com>
2025-02-07 12:29:39 -0800

    deleted unused method

--- lib/view/screens/home/components/app_bar.dart
+++ lib/view/screens/home/components/app_bar.dart
@@ -1,32 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-// Widget _title() {
-//   return Row(
-//     mainAxisAlignment: MainAxisAlignment.center,
-//     children: [
-//       const Icon(Icons.school, color: Colors.blueAccent, size: 32),
-//       const SizedBox(width: 8),
-//       ShaderMask(
-//         shaderCallback: (bounds) => const LinearGradient(
-//           colors: [Colors.blueAccent, Colors.purpleAccent, Colors.pinkAccent],
-//           begin: Alignment.topLeft,
-//           end: Alignment.bottomRight,
-//         ).createShader(bounds),
-//         child: const Text(
-//           'Study Buddy!!',
-//           style: TextStyle(
-//             fontFamily: 'Roboto', // Built-in system font
-//             fontSize: 35,
-//             fontWeight: FontWeight.bold,
-//             color: Colors.purpleAccent,
-//           ),
-//         ),
-//       ),
-//     ],
-//   );
-// }
-
 Widget _title() {
   return Row(
     mainAxisAlignment: MainAxisAlignment.center,
